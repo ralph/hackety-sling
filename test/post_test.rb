d6298c4f4c24531dd7c3b5552fdd18b3ebbe2b33
ralph <ralph@rvdh.de>
2011-08-12 10:04:26 +0200

    order everywhere ;)

--- test/post_test.rb
+++ test/post_test.rb
@@ -10,10 +10,10 @@ include Sinatra::HacketySling
 
 describe Post do
   it 'should return the correct date permalink' do
-    assert_equal '/2010/08/09/', Post.first.date_permalink
+    assert_equal '/2010/08/09/', Post.order_by(:title => :asc).first.date_permalink
   end
 
   it 'should return the correct permalink' do
-    assert_equal '/2010/08/09/test-post-1/', Post.first.permalink
+    assert_equal '/2010/08/09/test-post-1/', Post.order_by(:title => :asc).first.permalink
   end
 end
